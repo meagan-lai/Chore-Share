4a71569f058c0c2b18fb18213f5f3869d4fd8c83
Meagan Lai <Meagan@Meagans-MacBook-Air-2.local>
2018-12-27 18:50:05 -0500

    add and remove roomate functionality added

---
+++ App.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight
+} from "react-native";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      allRoomates: ["Meagan"]
+    };
+  }
+
+  roomateAdd(name) {
+    var allRoomates = this.state.allRoomates;
+    var newAllRoomates = allRoomates.concat(name);
+    if (allRoomates.includes(name)) {
+      var index = allRoomates.indexOf(name);
+      allRoomates.splice(index, 1);
+    } else {
+      this.setState({ allRoomates: newAllRoomates });
+      console.log(allRoomates);
+    }
+    console.log(allRoomates);
+  }
+  render() {
+    console.log(this.state.allRoomates);
+
+    return (
+      <View style={styles.container}>
+        <TouchableHighlight onPress={() => this.roomateAdd("Meagan")}>
+          <View>
+            <Text>Meagan</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={() => this.roomateAdd("Samantha")}>
+          <View>
+            <Text>Samantha</Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
+});
